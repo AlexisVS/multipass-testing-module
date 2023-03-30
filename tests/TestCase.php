52156104555482ee3687f74a019d61aede775ffa
AlexisVS <AlexisVS@users.noreply.github.com>
2023-03-30 12:41:32 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,13 +6,13 @@ use AlexisVS\MultipassTestingModule\MultipassTestingModuleServiceProvider;
 use File;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Laravel\Sanctum\SanctumServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use SplFileInfo;
 
 class TestCase extends Orchestra
 {
     use RefreshDatabase;
+
     /**
      * Automatically enables package discoveries.
      *
@@ -27,13 +27,12 @@ class TestCase extends Orchestra
      */
     protected $loadEnvironmentVariables = true;
 
-
     protected function setUp(): void
     {
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
-            fn(string $modelName) => 'AlexisVS\\MultipassTestingModule\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
+            fn (string $modelName) => 'AlexisVS\\MultipassTestingModule\\Database\\Factories\\'.class_basename($modelName).'Factory'
         );
     }
 
@@ -45,12 +44,6 @@ class TestCase extends Orchestra
 
 //        $migration = include __DIR__ . '/../database/migrations/create_testing_modules_table.php';
 //        $migration->up();
-
-
-
-
-
-
 
 //        $moduleMigrations = File::files(__DIR__ . '/../database/migrations');
 //        $applicationMigrations = collect(File::files(__DIR__ . '/../../../../database/migrations'));
@@ -72,11 +65,11 @@ class TestCase extends Orchestra
 
     public static function applicationBasePath(): string
     {
-        return __DIR__ . '/../../../../';
+        return __DIR__.'/../../../../';
     }
 
     protected function getBasePath(): string
     {
-        return __DIR__ . '/../../../../';
+        return __DIR__.'/../../../../';
     }
 }
