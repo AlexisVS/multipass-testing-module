d16c92b1f891a7c422e1d028ef55147d9010f018
AlexisVS <alexis.vansan1440@gmail.com>
2023-04-10 13:45:44 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,18 @@ use App\Models\Module;
 
 $module = Module::where('name', 'multipass-testing-module')->first();
 
-Route::get('module/'.$module->name, function () {
-    return new \Illuminate\Http\Response('Hello from Multipass Testing Module!', 200);
-})->name('module.'.$module->name.'.get');
+Route::name('module.' . $module->name)
+    ->prefix('module/' . $module->name)
+    ->group(function () use ($module) {
+        Route::get('/', function () {
+            return new \Illuminate\Http\Response('Hello from Multipass Testing Module!', 200);
+        })->name('module.' . $module->name);
 
-Route::name('module.'.$module->name)->prefix('module/'.$module->name)->group(function () {
-    Route::get('/', function () {
-        return new \Illuminate\Http\Response('Hello from Multipass Testing Module!', 200);
-    })->name('.get');
+        Route::post('test', function () {
+            return 'Hello from Multipass Testing Module!';
+        });
 
-    Route::post('test', function () {
-        return 'Hello from Multipass Testing Module!';
-    })->name('.post');
-
-    Route::get('page', function () {
-        return 'Hello from Multipass Testing Module!';
-    })->name('.get');
-});
+        Route::get('page', function () {
+            return 'Hello from Multipass Testing Module!';
+        });
+    });
