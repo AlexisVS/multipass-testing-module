4e9e2b8b519a12ca4ad3b4b7f794641f2ac5fc13
AlexisVS <alexis.vansan1440@gmail.com>
2023-04-10 15:20:19 +0200

    update

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -35,14 +35,6 @@ class TestCase extends Orchestra
     public function getEnvironmentSetUp($app): void
     {
         config()->set('database.default', 'testing');
-        config()->set('database.connections.testing', [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', 'mysql'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'testing'),
-            'username' => env('DB_USERNAME', 'sail'),
-            'password' => env('DB_PASSWORD', 'password'),
-        ]);
     }
 
     protected function getPackageProviders($app): array
