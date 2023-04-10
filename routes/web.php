91148112ec9ac5ade833c4ccbc4619145d51ceef
AlexisVS <alexis.vansan1440@gmail.com>
2023-04-10 15:13:17 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,16 @@ use App\Models\Module;
 
 $module = Module::where('name', 'multipass-testing-module')->first();
 
-Route::name('module.'.$module->name)
-    ->prefix('module/'.$module->name)
+Route::name('module.' . $module->name)
+    ->prefix('module/' . $module->name)
     ->group(function () use ($module) {
+
         Route::get('/', function () {
-            return new \Illuminate\Http\Response('Hello from Multipass Testing Module!', 200);
-        })->name('module.'.$module->name);
+            return new \Illuminate\Http\Response(
+                content: 'Hello from Multipass Testing Module!',
+                status: 200
+            );
+        });
 
         Route::post('test', function () {
             return 'Hello from Multipass Testing Module!';
