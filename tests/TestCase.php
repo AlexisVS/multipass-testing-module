ecf8d28127e7415e824afb1005fbd7df5a2c3092
AlexisVS <alexis.vansan1440@gmail.com>
2023-04-10 17:07:43 +0200

    update

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,7 +9,7 @@ use Orchestra\Testbench\TestCase as Orchestra;
 class TestCase extends Orchestra
 {
 //    use RefreshDatabase;
-    use InteractsWithDatabase;
+//    use InteractsWithDatabase; /** peut etre */
 
     /**
      * Automatically enables package discoveries.
@@ -44,5 +44,15 @@ class TestCase extends Orchestra
         return [
             MultipassTestingModuleServiceProvider::class,
         ];
+    }
+
+    public static function applicationBasePath(): string
+    {
+        return __DIR__.'/../../../../';
+    }
+
+    protected function getBasePath(): string
+    {
+        return __DIR__.'/../../../../';
     }
 }
