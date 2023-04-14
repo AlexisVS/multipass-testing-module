6f23ecaec6c97ae07e19f1711bcca07c1eb854b0
AlexisVS <alexis.vansan1440@gmail.com>
2023-04-14 14:15:06 +0200

    qzd

--- resources/js/Pages/Front.vue
+++ resources/js/Pages/Front.vue
@@ -3,13 +3,13 @@
         <hr>
         <h1>Module testing: Front page</h1>
         <hr>
-<!--        <TestComponent/>-->
+        <TestComponent/>
     </GuestLayout>
 </template>
 
 <script setup lang="ts">
 import GuestLayout from "@/Layouts/GuestLayout.vue";
-// import TestComponent from "@modules/multipass-testing-module/js/Components/TestComponent/TestComponent.vue";
+import TestComponent from "@modules/multipass-testing-module/js/Components/TestComponent/TestComponent.vue";
 </script>
 
 <style scoped>
