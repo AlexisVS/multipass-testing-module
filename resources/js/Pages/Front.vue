c49da9c6871c1873eed38843e065e90266ec5f3f
AlexisVS <alexis.vansan1440@gmail.com>
2023-04-14 15:12:24 +0200

    qzd

--- resources/js/Pages/Front.vue
+++ resources/js/Pages/Front.vue
@@ -4,12 +4,14 @@
         <h1>Module testing: Front page</h1>
         <hr>
         <TestComponent/>
+        <img :src="Image" alt="">
     </GuestLayout>
 </template>
 
 <script setup lang="ts">
 import GuestLayout from "@/Layouts/GuestLayout.vue";
 import TestComponent from "@modules/multipass-testing-module/js/Components/TestComponent/TestComponent.vue";
+import Image from '@img/handy-line-terminal-makes-card-payment.png'
 </script>
 
 <style scoped>
