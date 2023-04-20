57f8645a92f7e26ed1fa6a19daa8851ca97435f7
AlexisVS <AlexisVS@users.noreply.github.com>
2023-04-20 09:27:21 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -33,11 +33,11 @@ class TestCase extends Orchestra
 
     public static function applicationBasePath(): string
     {
-        return __DIR__ . '/../../../../';
+        return __DIR__.'/../../../../';
     }
 
     protected function getBasePath(): string
     {
-        return __DIR__ . '/../../../../';
+        return __DIR__.'/../../../../';
     }
 }
