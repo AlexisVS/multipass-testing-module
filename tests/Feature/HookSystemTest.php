c33fc3ec60ac1ec051fda70a5af27cfb6a65106d
AlexisVS <alexis.vansan1440@gmail.com>
2023-05-26 11:52:35 +0200

    fix github actions

--- tests/Feature/HookSystemTest.php
+++ tests/Feature/HookSystemTest.php
@@ -5,10 +5,6 @@ use Illuminate\Testing\TestResponse;
 it('can access to the module hook Component page', function () {
     /** @var TestResponse $request */
     $request = $this->get('module/multipass-testing-module/hook');
-<<<<<<< HEAD
-=======
-
->>>>>>> c8e0310af4da724aa24bae0dd0a9aec669d07770
     $request->assertOk();
 });
 
